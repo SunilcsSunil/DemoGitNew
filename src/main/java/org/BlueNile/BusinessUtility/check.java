04fce173918cd2766423a6842958e50acf3f29be
suni <sunilcs703@gmail.com>
2026-09-07 16:54:30 +0530

    again change

--- src/main/java/org/BlueNile/BusinessUtility/check.java
+++ src/main/java/org/BlueNile/BusinessUtility/check.java
@@ -3,7 +3,7 @@ package org.BlueNile.BusinessUtility;
 public class check {
 
 	public static void main(String[] args) {
-		System.out.println("Mummyyy");
+		System.out.println("MummyyyNaayi");
 
 	}
 
