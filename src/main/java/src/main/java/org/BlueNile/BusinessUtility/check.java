799effcae367354c414daf9bfdcc0a01414c3955
suni <sunilcs703@gmail.com>
2026-09-07 17:09:05 +0530

    commit form stuff

---
+++ src/main/java/src/main/java/org/BlueNile/BusinessUtility/check.java
@@ -0,0 +1,11 @@
+package src.main.java.org.BlueNile.BusinessUtility;
+
+public class check {
+
+	public static void main(String[] args) {
+		System.out.println("MummyyyNaayi");
+		System.out.println("DummyNaayi");
+
+	}
+
+}
